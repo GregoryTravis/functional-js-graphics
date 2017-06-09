db53d87795a77beb0b195c81d0e97b8a954f90e4
Gregory Travis <greg.m.travis@gmail.com>
2017-06-09 18:51:42 -0400

    init

---
+++ vo.js
@@ -0,0 +1,13 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var imgData = ctx.createImageData(100, 100);
+
+var i;
+for (i = 0; i < imgData.data.length; i += 4) {
+    imgData.data[i+0] = 255;
+    imgData.data[i+1] = 0;
+    imgData.data[i+2] = 0;
+    imgData.data[i+3] = 255;
+}
+
+ctx.putImageData(imgData, 10, 10);
